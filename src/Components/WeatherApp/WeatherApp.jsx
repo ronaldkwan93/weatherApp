526fdf4f46090652e891dc5c4ac178ac67117d21
Ronald Kwan <ronaldkwan93@gmail.com>
2024-03-19 19:37:59 +1100

    api and url setup

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,11 +11,23 @@ import wind_icon from '../Assets/wind.png'
 import humidity_icon from '../Assets/humidity.png'
 
 const WeatherApp = () => {
+
+    let api_key = "ba11d5e609ce0fccf94514f41fbf5842"
+
+    const search= () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value === "") 
+        {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`
+    }
+
   return (
     <div className='container'>
       <div className="top-bar">
         <input type="text" className="cityInput"placeholder='search'/>
-        <div className="search-icon">
+        <div className="search-icon" onClick={() => {search()}}>
             <img src={search_icon} alt="" />
         </div>
       </div>
